3df3a05a32f588e872a89142fb197535037e6b5d
Aaron Bassett <aaron.bassett@nantomics.com>
2020-01-14 17:53:11 -0500

    intial commit phonehome class for checking range

---
+++ phonehome.py
@@ -0,0 +1,42 @@
+import time
+import machine
+from uPySensors.ssd1306_i2c import Display
+
+
+
+class Wanderer:
+    def __init__(self, lora):
+        print("LoRa Wanderer")
+        self.lora = lora
+        self.display = Display()
+        self.current_packet = 0
+        lora.onReceive(self.listen)
+        lora.onTimeout(self.timeout)
+
+    def listen(self, lora, payload):
+        msg = "OK!! {}    RSSI {}".format(str(payload), self.lora.packetRssi())
+        print(msg)
+        self.display.show_text_wrap(msg)
+        time.sleep(1)
+        self.phone()
+        
+
+    def timeout(self, lora, payload):
+        msg = "MISSING RESPONSE FOR PACKET {}".format(self.current_packet)
+        print(msg)
+        self.display.show_text_wrap(msg)
+        time.sleep(1)
+        self.phone()
+
+    def phone(self):
+        msg = "PHONING ATTEMPT NUMBER {}".format(self.current_packet)
+        self.lora.println(msg)
+        print(msg)
+        self.lora.receive_single()
+        # self.display.show_text_wrap(msg)
+        self.current_packet += 1
+
+
+
+
+
